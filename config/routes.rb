a31d83e03466392a7afc7878127a2949bf49ac0b
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-03-18 09:41:24 +0530

    Added devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
+
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +10,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "devise/sessions#new"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
